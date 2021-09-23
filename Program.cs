539de54cbe328f661d97134e287549ea71ff2f16
LuisSid55 <luiscitty4@gmail.com>
2021-09-23 03:37:50 -0300

    Cebolinha

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Texto;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Digite seu texto: ");
+            Texto = Console.ReadLine();
+            Texto = Texto.Replace("R", "L");
+            Texto = Texto.Replace("r", "l");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Cebolinha diz:    {Texto}");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
